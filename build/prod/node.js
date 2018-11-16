86e987dd6831e2f04d4b0b66412db80a5c378849
yanguoyu <yanguoyu@betalpha.com>
2018-11-16 15:41:45 +0800

    1：build prod node。拷贝到结果文件夹。

---
+++ build/prod/node.js
@@ -0,0 +1,20 @@
+const Npm = require('npm-shell');
+const path = require('path');
+const fs = require('fs');
+const nodeModulesPath = './node_modules';
+
+const nodePath = path.resolve(process.cwd(), './node/');
+const nodeNpm = new Npm(nodePath);
+// 清空非追踪文件
+nodeNpm.shell('git', ['clean', '-xfd']);
+// 下载node_modules
+if(!fs.existsSync(path.resolve(nodePath, nodeModulesPath))) {
+  nodeNpm.install('--production');
+}
+// 打包拷贝到main
+const buildDir = `${path.resolve(process.cwd(), './node/.')}/.`;
+const dstDir = path.resolve(process.cwd(), './main/node/');
+// 删除render dist
+nodeNpm.shell('rm',['-rf', dstDir]);
+// 拷贝render build 到dist
+nodeNpm.shell('cp',['-r', buildDir, dstDir]);
--- build/prod/render.js
+++ build/prod/render.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const nodeModulesPath = './node_modules';
 
-// 启动 render
 const renderPath = path.resolve(process.cwd(), './render/');
 const renderNpm = new Npm(renderPath);
 if(!fs.existsSync(path.resolve(renderPath, nodeModulesPath))) {
@@ -11,6 +10,8 @@ if(!fs.existsSync(path.resolve(renderPath, nodeModulesPath))) {
 }
 const buildDir = `${path.resolve(process.cwd(), './render/build/.')}/.`;
 const dstDir = path.resolve(process.cwd(), './main/render/');
+// 删除render dist
 renderNpm.shell('rm',['-rf', dstDir]);
 renderNpm.run('build');
+// 拷贝render build 到dist
 renderNpm.shell('cp',['-r', buildDir, dstDir]);
