53b2cca9e546bf1dd38ec851069ac99b37c97960
yanguoyu <yanguoyu@betalpha.com>
2018-11-27 13:28:05 +0800

    1：加上日志。

--- main/main.js
+++ main/main.js
@@ -1,26 +1,35 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import log from 'electron-log';
 import isDevelopment from './enviroment';
 import { startNode, stopNode } from './node-run';
 
 let mainWindow
+log.transports.file.level = 'silly';
+log.warn('enter main.js');
 
 function createWindow () {
+  log.info('create window');
   mainWindow = new BrowserWindow({width: 800, height: 600})
   if(isDevelopment) {
     mainWindow.loadURL('localhost:3000');
   } else {
+    log.info(`file://${path.resolve(__dirname, './render', 'index.html')}`);
     mainWindow.loadURL(`file://${path.resolve(__dirname, './render', 'index.html')}`);
     startNode();
   }
   mainWindow.on('closed', async function () {
     mainWindow = null
+    if(!isDevelopment) {
+      stopNode();
+    }
   })
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
+  log.info('window-all-closed');
   if (process.platform !== 'darwin') {
     app.quit()
   }
--- main/node-run.js
+++ main/node-run.js
@@ -1,13 +1,19 @@
+import log from 'electron-log';
 const Npm = require('npm-shell');
 const path = require('path');
-const nodePath = path.resolve(process.cwd(), './node/');
+const nodePath = `file://${path.resolve(__dirname, '../../node/')}`;
 const nodeNpm = new Npm(nodePath);
 
 // 启动node
 export const startNode = () => {
+  log.info(nodePath);
+  log.info('start node begin');
   nodeNpm.run('start');
+  log.info('start node success');
 }
 
 export const stopNode = () => {
+  log.info('stop node begin');
   nodeNpm.run('stop');
+  log.info('stop node success');
 }
